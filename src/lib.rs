5f743ae83b0c685c165e2f17a8dd2ae16c0044ed
amigin <amigin@gmail.com>
2022-08-08 11:28:08 +0300

    Added WebSocketMessage

--- src/lib.rs
+++ src/lib.rs
@@ -2,5 +2,5 @@ mod helpers;
 mod my_web_socket;
 mod my_web_socket_callback;
 pub use helpers::handle_web_socket_upgrade;
-pub use my_web_socket::MyWebSocket;
+pub use my_web_socket::*;
 pub use my_web_socket_callback::MyWebSockeCallback;
